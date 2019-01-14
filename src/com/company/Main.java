8dd0b6394addf7a11cede3457fffc95a1509c120
tk304 <tk304@pvsd.org>
2019-01-14 11:22:23 -0500

    zgjlreAFBkaefkmef

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,41 @@
 package com.company;
+import java.util.*;
 
 public class Main {
 
+    public static boolean TryParse(String a)
+    {
+        try
+        {
+            Integer.parseInt(a);
+            return true;
+        }
+        catch (Exception x)
+        {
+            return false;
+        }
+    }
+
+    public static TowersOfHanoi PromptInput()
+    {
+        Scanner scan = new Scanner(System.in);
+
+        String in = scan.nextLine();
+        if (TryParse(in))
+        {
+            return new TowersOfHanoi(Integer.parseInt(in));
+        }
+        else
+        {
+            System.out.println("Input is not a number!");
+            PromptInput();
+        }
+    }
+
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.print("Enter number of blocks: ");
+        TowersOfHanoi toh = PromptInput();
+
     }
 }
---
+++ src/com/company/TowersOfHanoi.java
@@ -0,0 +1,20 @@
+package com.company;
+
+/**
+ * Created by tk304 on 1/14/19.
+ */
+public class TowersOfHanoi {
+
+    private static int size;
+
+    public TowersOfHanoi(int size)
+    {
+        this.size = size;
+    }
+
+    public void Solve()
+    {
+
+    }
+
+}
